79d1eba4da38fa0ef73edc3e4b1e1f17740944dc
Pavel Kopanev <kopanev-pavel-spb@yandex.ru>
2020-05-27 19:38:35 +0300

    Simple CarRacing env works (with video recorder)

--- src/reinforcement_learning/env/README.md
+++ src/reinforcement_learning/env/README.md
@@ -1,2 +1,5 @@
 ~/anaconda3/envs/gym_env/bin/pip install gym[atari]
-~/anaconda3/envs/gym_env/bin/pip install box2d-py
+
+~/anaconda3/envs/gym_env/bin/pip install box2d-py 
+
+~/anaconda3/envs/gym_env/bin/pip install pyglet==v1.3.2 (pyglet-1.5.5 for simple simulation)
--- src/reinforcement_learning/test_launch_car_racing.py
+++ src/reinforcement_learning/test_launch_car_racing.py
@@ -3,14 +3,14 @@ import random
 import numpy as np
 from pyglet.window import key
 import src.reinforcement_learning.env
-from src.reinforcement_learning.env.gym_core_environments.car_racing_advanced.car_racing import play
 
 random.seed(0)  # make results reproducible
-advanced_mode = False
+advanced_mode = True
+record_video = False
 
 if advanced_mode:
     env = gym.make('CarRacingCustomAdvanced-v0',
-                   allow_reverse=False,
+                   allow_reverse=True,
                    grayscale=1,
                    show_info_panel=1,
                    discretize_actions=None,
@@ -20,7 +20,82 @@ if advanced_mode:
                    max_time_out=0,
                    frames_per_state=4,
                    num_obstacles=10)
-    play(env)
+
+    discretize = env.discretize_actions
+    if discretize is None:
+        a = np.array([0.0, 0.0, 0.0])
+    else:
+        a = np.array([0])
+
+
+    def key_press(k, mod):
+        global restart
+        if discretize is None:
+            if k == 0xff0d: restart = True
+            if k == key.LEFT:  a[0] = -1.0
+            if k == key.RIGHT: a[0] = +1.0
+            if k == key.UP:    a[1] = +1.0
+            if k == key.DOWN:  a[1] = -1.0
+            if k == key.SPACE: a[2] = +0.8  # set 1.0 for wheels to block to zero rotation
+        elif discretize == "hard":
+            if k == 0xff0d: restart = True
+            if k == key.LEFT:  a[0] = 1
+            if k == key.RIGHT: a[0] = 2
+            if k == key.UP:    a[0] = 3
+            if k == key.SPACE: a[0] = 4
+
+
+    def key_release(k, mod):
+        if discretize is None:
+            if k == key.LEFT and a[0] == -1.0: a[0] = 0
+            if k == key.RIGHT and a[0] == +1.0: a[0] = 0
+            if k == key.UP:    a[1] = 0
+            if k == key.DOWN:  a[1] = 0
+            if k == key.SPACE: a[2] = 0
+        else:
+            a[0] = 0
+        if k == key.D:     set_trace()
+        if k == key.R:     env.reset()
+        if k == key.Z:     env.change_zoom()
+        if k == key.G:     env.switch_intersection_groups()
+        if k == key.I:     env.switch_intersection_points()
+        if k == key.X:     env.switch_xt_intersections()
+        if k == key.E:     env.switch_end_of_track()
+        if k == key.S:     env.switch_start_of_track()
+        if k == key.T:     env.screenshot('./')
+        if k == key.Q:     sys.exit()
+
+
+    env.render()
+    record_video = False
+    if record_video:
+        env.monitor.start('/tmp/video-test', force=True)
+    env.key_press_fn = key_press
+    env.key_release_fn = key_release
+    while True:
+        env.reset()
+        total_reward = 0.0
+        steps = 0
+        restart = False
+
+        while True:
+            if discretize is not None:
+                a_tmp = a[0]
+            else:
+                a_tmp = a
+            s, r, done, info = env.step(a_tmp)
+            total_reward += r
+            if steps % 200 == 0 or done:
+                # print("\naction " + str(["{:+0.2f}".format(x) for x in a]))
+                print("step {} total_reward {:+0.2f}".format(steps, total_reward))
+                steps += 1
+                print("step {} total_reward {:+0.2f}".format(steps, total_reward))
+            steps += 1
+            if not record_video:  # Faster, but you can as well call env.render() every time to play full window.
+                env.render()
+            if done or restart: break
+
+    env.close()
 
 else:
     env = gym.make('CarRacingCustom-v0')
@@ -42,10 +117,9 @@ else:
     env.render()
     env.viewer.window.on_key_press = key_press
     env.viewer.window.on_key_release = key_release
-    record_video = False
     if record_video:
         from gym.wrappers.monitor import Monitor
-        env = Monitor(env, '/tmp/video-test', force=True)
+        env = Monitor(env, '/home/pavel/Skoltech/DL/final_project/auto-features-extraction-for-RL/src/reinforcement_learning/env/video-test', force=True)
     isopen = True
     while isopen:
         env.reset()
