faa80d43ca5489062f34150aeea6d901887aa41b
Pavel Kopanev <kopanev-pavel-spb@yandex.ru>
2020-05-28 14:12:32 +0300

    Slight fixes

--- src/reinforcement_learning/test_launch_donkeycar.py
+++ src/reinforcement_learning/test_launch_donkeycar.py
@@ -3,9 +3,11 @@ import gym
 import gym_donkeycar
 import numpy as np
 import matplotlib.pyplot as plt
+from pyglet.window import key
 
 #%% SET UP ENVIRONMENT
 exe_path = f"env/third_party_environments/gym-donkeycar/apps/donkey_sim.x86_64"
+# exe_path = ""
 port = 9091
 
 env_list = [
@@ -18,14 +20,19 @@ env_list = [
 
 env = gym.make(env_list[-1], exe_path=exe_path, port=port)
 
+key_handler = key.KeyStateHandler()
+# env.unwrapped.window.push_handlers(key_handler)
+
 #%% PLAY
 obv = env.reset()
 for t in range(100):
-    action = np.array([0.0, 0.5])  # drive straight with small speed
+    # action = np.array([0.0, 0.5])  # drive straight with small speed
+
+    action = np.array([0.0, 0.0])
 
     # execute the action
     obv, reward, done, info = env.step(action)
     print(type(obv))
     print(obv.shape)
-    plt.imshow(obv)
-    plt.show()
+    # plt.imshow(obv)
+    # plt.show()
