695db9c471bdccea761d6c0a0e55bacf0cd48808
Pavel Kopanev <kopanev-pavel-spb@yandex.ru>
2020-05-28 18:15:44 +0300

    Data generation from Unity and saving added for DonkeyCar

--- src/reinforcement_learning/env/README.md
+++ src/reinforcement_learning/env/README.md
@@ -3,6 +3,8 @@
 ```sh
 ~/anaconda3/envs/gym_env/bin/pip install box2d-py
 
+~/anaconda3/envs/gym_env/bin/pip install manage.py
+
 ~/anaconda3/envs/gym_env/bin/pip install pyglet==v1.3.2 (pyglet-1.5.5 for simple simulation)
 
 ~/anaconda3/envs/gym_env/bin/pip install . (in submodule directories for installation of the third party environments)
--- src/reinforcement_learning/test_launch_donkeycar.py
+++ src/reinforcement_learning/test_launch_donkeycar.py
@@ -3,12 +3,58 @@ import gym
 import gym_donkeycar
 import numpy as np
 import matplotlib.pyplot as plt
-from pyglet.window import key
+import sys
+import threading
+from PIL import Image
 
-#%% SET UP ENVIRONMENT
-exe_path = f"env/third_party_environments/gym-donkeycar/apps/donkey_sim.x86_64"
-# exe_path = ""
-port = 9091
+
+action = np.array([0.0, 0.0])
+
+NUM_EPISODES = 3
+MAX_TIME_STEPS = 1000000
+
+
+def select_action(env):
+    return env.action_space.sample()
+
+
+def simulate(env):
+
+    for episode in range(NUM_EPISODES):
+
+        # Reset the environment
+        obv = env.reset()
+
+        for t in range(MAX_TIME_STEPS):
+
+            # Select an action
+            action = select_action(env)
+            # action = np.array([0.0, 0.0])
+
+            # execute the action
+            # observation, reward, done, info = env.step(action)
+            observation, reward, done, info = env.viewer.observe()
+
+            wrapped_image = observation.transpose(2, 0, 1)
+            print(type(wrapped_image))
+            print(wrapped_image.shape)
+
+            if t % 25 == 0:
+                # plt.imshow(observation)
+                # plt.show()
+                im = Image.fromarray(observation)
+                im.save('data/frame_' + str(t) + '.jpeg')
+
+            '''
+            if done:
+                break
+                '''
+
+
+# SET UP ENVIRONMENT
+# exe_path = f"env/third_party_environments/gym-donkeycar/apps/donkey_sim.x86_64"
+exe_path = ""
+port = 9090
 
 env_list = [
             "donkey-warehouse-v0",
@@ -20,19 +66,6 @@ env_list = [
 
 env = gym.make(env_list[-1], exe_path=exe_path, port=port)
 
-key_handler = key.KeyStateHandler()
-# env.unwrapped.window.push_handlers(key_handler)
-
-#%% PLAY
-obv = env.reset()
-for t in range(100):
-    # action = np.array([0.0, 0.5])  # drive straight with small speed
-
-    action = np.array([0.0, 0.0])
-
-    # execute the action
-    obv, reward, done, info = env.step(action)
-    print(type(obv))
-    print(obv.shape)
-    # plt.imshow(obv)
-    # plt.show()
+simulate(env)
+env.close()
+print("test finished")
