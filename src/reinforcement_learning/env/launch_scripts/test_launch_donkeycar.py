5563f975019708a7ec4f48aae0d53537a8c2e79c
Pavel Kopanev <kopanev-pavel-spb@yandex.ru>
2020-05-30 15:47:22 +0300

    New Unity build added

--- src/reinforcement_learning/env/launch_scripts/test_launch_donkeycar.py
+++ src/reinforcement_learning/env/launch_scripts/test_launch_donkeycar.py
@@ -64,8 +64,8 @@ def simulate(env):
             # action = np.array([0.0, 0.0])
 
             # execute the action
-            # observation, reward, done, info = env.step(action)
-            observation, reward, done, info = env.viewer.observe()
+            observation, reward, done, info = env.step(action)
+            # observation, reward, done, info = env.viewer.observe()
 
             wrapped_image = observation.transpose(2, 0, 1)
             # print(type(wrapped_image))
@@ -73,32 +73,34 @@ def simulate(env):
             print(reward)
             rewards.append(reward)
 
-            if (t+1) % 400 == 0:
+            if t % 100 == 0:
                 plt.plot(rewards)
-                # plt.show()
+                plt.show()
+                plt.imshow(observation)
+                plt.show()
 
-            # if t % 25 == 0:
-                # plt.imshow(observation)
-                # plt.show()
-            im = Image.fromarray(observation)
-            PATH = os.path.abspath('env/') + '/data_samples/custom_environments/donkeycar/'
-            drive_file = drive.CreateFile({'title': ENV_NAME[0] + '_fisheye' + '_time_step_' + str(t) + '.jpg',
-                                            'parents': [{'id': '1iBxcuGkYQlJmNTRbdBzPcUjjh2liy_Rt'}]})
-            file_name = PATH + ENV_NAME[0] + '_fisheye' + '_time_step_' + str(t) + '.jpg'
-            im.save(file_name)
-            drive_file.SetContentFile(file_name)
-            drive_file.Upload()
-            os.remove(file_name)
+            '''
+            if t % 25 == 0:
+                plt.imshow(observation)
+                plt.show()
+                im = Image.fromarray(observation)
+                PATH = os.path.abspath('env/') + '/data_samples/custom_environments/donkeycar/'
+                drive_file = drive.CreateFile({'title': ENV_NAME[0] + '_fisheye' + '_time_step_' + str(t) + '.jpg',
+                                                'parents': [{'id': '1iBxcuGkYQlJmNTRbdBzPcUjjh2liy_Rt'}]})
+                file_name = PATH + ENV_NAME[0] + '_fisheye' + '_time_step_' + str(t) + '.jpg'
+                im.save(file_name)
+                drive_file.SetContentFile(file_name)
+                drive_file.Upload()
+                os.remove(file_name)'''
 
             '''
             if done:
-                break
-                '''
+                break'''
 
 
 # SET UP ENVIRONMENT
-# exe_path = os.path.abspath('env/') + "/third_party_environments/gym-donkeycar/apps/donkey_sim.x86_64"
-exe_path = ""
+exe_path = os.path.abspath('env/') + "/third_party_environments/gym-donkeycar/apps/donkey_sim_custom_build.x86_64"
+# exe_path = ""  # If you have running game in Unity
 port = 9090
 
 env_list = [
@@ -109,7 +111,7 @@ env_list = [
     "donkey-mountain-track-v0"
 ]
 
-env = gym.make(env_list[-1], exe_path=exe_path, port=port)
+env = gym.make(env_list[0], exe_path=exe_path, port=port)
 
 simulate(env)
 env.close()
